f6b14cce1d304bd087ee7d7c1868b3ec41a6e95f
yehor <kravtsov.egor@kronas.com.ua>
2025-02-06 18:37:43 +0100

    Run basic Vue Data Grid

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import '@/styles/settings/index.scss'
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
 import store from './store'
 import router from './router'
--- src/views/TheTable.vue
+++ src/views/TheTable.vue
@@ -1,10 +1,28 @@
 <script setup>
 import { useStore } from 'vuex'
-import { computed, onMounted } from 'vue'
+import { computed, onMounted, ref } from 'vue'
 import types from '@/store/modules/consumers/types.js'
+import { AgGridVue } from 'ag-grid-vue3'
+import { AllCommunityModule, ModuleRegistry } from 'ag-grid-community'
+
+ModuleRegistry.registerModules([AllCommunityModule])
 
 const store = useStore()
 const consumers = computed(() => store.state.consumers[types.CONSUMERS_STATE])
+
+const colDefs = ref([
+  { field: 'customer_id' },
+  { field: 'full_name' },
+  { field: 'date_of_birth' },
+  { field: 'email' },
+  { field: 'phone_number' },
+  { field: 'address' },
+  { field: 'city' },
+  { field: 'state' },
+  { field: 'country' },
+  { field: 'zip_code' },
+  { field: 'employment_status' },
+])
 
 onMounted(() => {
   store.dispatch(`consumers/${types.LOAD_CONSUMERS_ACTION}`)
@@ -14,10 +32,6 @@ onMounted(() => {
 <template>
   <div>
     <h1>Consumers</h1>
-    <ul>
-      <li v-for="consumer in consumers" :key="consumer.customer_id">
-        {{ consumer.full_name }}
-      </li>
-    </ul>
+    <ag-grid-vue :rowData="consumers" :columnDefs="colDefs" style="height: 500px"> </ag-grid-vue>
   </div>
 </template>
