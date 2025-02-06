544840d3d1f4ed1ed212e1f33e14dfeeb883f5b4
yehor <kravtsov.egor@kronas.com.ua>
2025-02-06 18:23:28 +0100

    Configured fonts, normalised styles and set scss supporting

--- src/components/Layouts/TheHeader.vue
+++ src/components/Layouts/TheHeader.vue
@@ -1,5 +1,5 @@
 <template>
-  <header>
+  <header class="header">
     <div class="header__wrapper">
       <router-link :to="routes.TABLE">Table</router-link>
       <router-link :to="routes.CHARTS">Charts</router-link>
@@ -11,17 +11,15 @@
 import routes from "@/router/routes.js"
 </script>
 
-<style scoped>
-header {
-  margin-top: 10vh;
-  margin-bottom: 5vh;
+<style lang="scss" scoped>
+.header {
   padding-block: 1.6rem;
-}
 
-.header__wrapper {
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  gap: 1.6rem;
+  &__wrapper {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    gap: 1.6rem;
+  }
 }
 </style>
