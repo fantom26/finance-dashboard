e1a242bbe9ad26250c0f373545d8ecbf60dafe8b
yehor <kravtsov.egor@kronas.com.ua>
2025-02-11 20:57:22 +0100

    Added pie chart and set amcharts lib

---
+++ src/components/charts/PieChart.vue
@@ -0,0 +1,41 @@
+<script setup>
+import { onMounted } from 'vue'
+import * as am5 from '@amcharts/amcharts5'
+import * as am5percent from '@amcharts/amcharts5/percent'
+
+const CHART_ID = 'pie-chart-div'
+
+onMounted(() => {
+  const root = am5.Root.new(CHART_ID)
+
+  const chart = root.container.children.push(am5percent.PieChart.new(root, {}))
+
+  const series = chart.series.push(
+    am5percent.PieSeries.new(root, {
+      valueField: 'value',
+      categoryField: 'category',
+    }),
+  )
+
+  series.data.setAll([
+    {
+      category: 'Research',
+      value: 1000,
+    },
+    {
+      category: 'Marketing',
+      value: 1200,
+    },
+    {
+      category: 'Sales',
+      value: 850,
+    },
+  ])
+
+  return () => root.dispose()
+})
+</script>
+
+<template>
+  <div :id="CHART_ID" style="width: 100%; height: 400px"></div>
+</template>
--- src/views/TheCharts.vue
+++ src/views/TheCharts.vue
@@ -2,9 +2,11 @@
   <section>
     <the-container>
       <h2>Charts page</h2>
+      <pie-chart />
     </the-container>
   </section>
 </template>
 <script setup>
 import TheContainer from '@/components/base/BaseContainer.vue'
+import PieChart from '@/components/charts/PieChart.vue'
 </script>
