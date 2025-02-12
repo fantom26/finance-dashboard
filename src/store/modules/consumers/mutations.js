c1007fc9ef2863189cce76ffda79d130949fb401
yehor <kravtsov.egor@kronas.com.ua>
2025-02-12 11:44:44 +0100

    Updated function for calculation statistics for charts

--- src/store/modules/consumers/mutations.js
+++ src/store/modules/consumers/mutations.js
@@ -3,39 +3,49 @@ import types from './types.js'
 export default {
   [types.SET_CONSUMERS_MUTATION](state, payload) {
     state[types.CONSUMERS_STATE] = payload
-    state[types.TRANSACTION_TYPE_DISTRIBUTION_STATE] = formatTransactionData(
+    state[types.TRANSACTION_TYPE_DISTRIBUTION_STATE] = formatData(
       payload,
       'transaction_type',
       'amount',
+      countConsumerByTransactionInfo,
     )
-    state[types.TRANSACTION_LOCATION_STATE] = formatTransactionData(
+    state[types.TRANSACTION_LOCATION_STATE] = formatData(
       payload,
       'transaction_location',
       'amount',
+      countConsumerByTransactionInfo,
     )
-    state[types.CONSUMERS_GENDER_STATE] = formatTransactionData(
-      payload,
-      'transaction_location',
-      'amount',
-    )
+    state[types.CONSUMERS_GENDER_STATE] = formatData(payload, 'gender', null, countEntityByProperty)
   },
 }
 
-function formatTransactionData(consumers, category, value) {
+function countEntityByProperty(category, value, summary, consumer) {
+  const type = consumer[category]
+  if (!Object.prototype.hasOwnProperty.call(summary, type)) {
+    summary[type] = 0
+  }
+  summary[type] += 1
+  return summary
+}
+
+function countConsumerByTransactionInfo(category, value, summary, consumer) {
+  const type = consumer.transactions[category]
+  if (!Object.prototype.hasOwnProperty.call(summary, type)) {
+    summary[type] = 0
+  }
+  summary[type] += consumer.transactions[value]
+  return summary
+}
+
+function formatData(consumers, category, value, strategy) {
   if (!consumers.length) return []
 
-  const transactionSummary = {}
+  const boundStrategy = strategy.bind(this, category, value)
 
-  consumers.forEach((consumer) => {
-    const type = consumer.transactions[category]
-    if (!transactionSummary[type]) {
-      transactionSummary[type] = 0
-    }
-    transactionSummary[type] += consumer.transactions[value]
-  })
+  const summary = consumers.reduce(boundStrategy, {})
 
-  return Object.keys(transactionSummary).map((key) => ({
+  return Object.keys(summary).map((key) => ({
     category: key,
-    value: transactionSummary[key],
+    value: summary[key],
   }))
 }
