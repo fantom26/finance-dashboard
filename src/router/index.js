a971aa33f67a0df351862751d872428b2c6175c2
yehor <kravtsov.egor@kronas.com.ua>
2025-02-20 11:14:40 +0100

    Added lazy loading for charts page and notFound

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,4 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import NotFound from '@/views/NotFound.vue'
-import TheCharts from '@/views/TheCharts.vue'
 import TheTable from '@/views/TheTable.vue'
 import routes from './routes'
 
@@ -8,9 +6,9 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     { path: '/', redirect: routes.TABLE },
-    { path: routes.TABLE, component: TheTable, meta: { transition: 'slide-left' } },
-    { path: routes.CHARTS, component: TheCharts, meta: { transition: 'slide-right' } },
-    { path: routes.NOT_FOUND, component: NotFound },
+    { path: routes.TABLE, component: TheTable },
+    { path: routes.CHARTS, component: () => import('@/views/TheCharts.vue') },
+    { path: routes.NOT_FOUND, component: () => import('@/views/NotFound.vue') },
   ],
 })
 
