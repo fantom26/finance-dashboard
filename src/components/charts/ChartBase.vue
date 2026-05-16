29253d91d473998038ae57496767fc3b77aae438
kravtsovyehor <kravtsovyehor@gmail.com>
2026-05-16 20:46:01 +0200

    Add ChartBase.vue

---
+++ src/components/charts/ChartBase.vue
@@ -0,0 +1,11 @@
+<template>
+  <div ref="chartRef" style="width: 100%; height: 100%"></div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const chartRef = ref<HTMLElement | null>(null)
+
+defineExpose({ chartRef })
+</script>
