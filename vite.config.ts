f4402ca9d93b1891438fedf43abb6ad5ed4d8a45
kravtsovyehor <kravtsovyehor@gmail.com>
2026-05-16 14:27:26 +0200

    Refactor lint-staged configuration for improved file handling and update Vite config to use path aliasing for better module resolution

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,17 @@
-import { fileURLToPath, URL } from 'node:url'
 import VueRouter from 'vue-router/vite'
 
-import { defineConfig } from 'vite'
+import { type AliasOptions, defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path'
+
+const root = path.resolve(__dirname, 'src')
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    VueRouter({}),
-    vue()],
+  plugins: [VueRouter({}), vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-    },
+      '@': root,
+    } as AliasOptions,
   },
 })
